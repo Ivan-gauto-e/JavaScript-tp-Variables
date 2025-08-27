9c31e1c2ce24e591f410b98a0127b69244f8750c
Ivan Gauto <ivan.gauto@gm2dev.com>
2025-08-27 09:52:41 -0300

    Resolucion del segundo ejercicio de javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+// Resolución del primer ejercicio
+let A = 10;
+let B = 5;
+if (A > B) {
+  console.log("A es mayor que B");
+}
+
+// Resolución del segundo ejercicio
+let ref = 8;
+let edad = 18;
+if (edad > ref) {
+  console.log(`Eres mayor de ${ref} años`);
+}
