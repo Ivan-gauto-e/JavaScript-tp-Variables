2478f1019c66d530750aad3580ebb017769c5810
Ivan Gauto <ivan.gauto@gm2dev.com>
2025-08-27 10:16:33 -0300

    Resolucion de los ejercicios if del tp de javascript

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ let edad = 18;
 if (edad > ref) {
   console.log(`Eres mayor de ${ref} años`);
 }
+
+// Resolución del tercer ejercicio
+let Numero1 = 7;
+let Numero2 = 2;
+if (Numero1 > Numero2) {
+  console.log("Numero1 > Numero2");
+}
+
+// Resolucion del cuarto ejercicio
+let Nombre = "Juan";
+let Nombre2 = "Pedro";
+if (Nombre === Nombre2) {
+  console.log("Juan");
+}
+
+// Resolución del quinto ejercicio
+if (Nombre !== Nombre2) {
+  console.log("Los nombres son diferentes");
+}
+
+// Resolución del sexto ejercicio
+if (Numero2 < 0) {
+  console.log("Numero2 es negativo");
+}
+
+// Resolución del séptimo ejercicio
+if (Numero1 > 0) {
+  console.log("Numero1 es positivo");
+}
+
+// Resolución del octavo ejercicio
+if (Numero1 % 2 === 0) {
+  console.log("Numero1 es par");
+} else {
+  console.log("Numero1 es impar");
+}
